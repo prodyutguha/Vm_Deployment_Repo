d9909db20125aaa1055e619561bb378456593e7e
John Doe <prodyut@kloudgurdian.com>
2025-05-27 13:55:14 +0530

    test for code

--- provider.tf
+++ provider.tf
@@ -1,21 +1,21 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.10.0"
-    }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 4.10.0"
+#     }
 
-    random = {
-      source = "hashicorp/random"
-      version = "3.4.3"
-    }
+#     random = {
+#       source = "hashicorp/random"
+#       version = "3.4.3"
+#     }
 
-    tls = {
-      source = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-  }
-}
+#     tls = {
+#       source = "hashicorp/tls"
+#       version = "~> 4.0.4"
+#     }
+#   }
+# }
 
 
 
@@ -36,3 +36,19 @@ terraform {
 #   #tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
 #   #subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
 # }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
