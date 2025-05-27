486cfb0e53fde3c4a960fb8b0e75af4ef2c21c25
prodyutguha <85899706+prodyutguha@users.noreply.github.com>
2025-05-27 12:53:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -31,8 +31,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "5cfd5092-8ede-4841-b3fb-56245200ab93"
-  client_secret   = "g1F8Q~MaYT8N50lYOJ~LwyzPkGlrX.0y4fgbQa8g"
-  tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
-  subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
+  #client_id       = "5cfd5092-8ede-4841-b3fb-56245200ab93"
+  #client_secret   = "g1F8Q~MaYT8N50lYOJ~LwyzPkGlrX.0y4fgbQa8g"
+  #tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
+  #subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
 }
