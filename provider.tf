5964ad77c44b66b0e8d67fdabb701858710aba35
John Doe <prodyut@kloudgurdian.com>
2025-05-27 13:04:17 +0530

    test

--- provider.tf
+++ provider.tf
@@ -1,38 +1,38 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = ">= 4.14.0"
-#     }
-
-#     random = {
-#       source = "hashicorp/random"
-#       version = "3.4.3"
-#     }
-
-#     tls = {
-#       source = "hashicorp/tls"
-#       version = "~> 4.0.4"
-#     }
-#   }
-# }
-
-
-
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">=4.10.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 4.14.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
 
-  #client_id       = "5cfd5092-8ede-4841-b3fb-56245200ab93"
-  #client_secret   = "g1F8Q~MaYT8N50lYOJ~LwyzPkGlrX.0y4fgbQa8g"
-  #tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
-  #subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
-}
+
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = ">=4.10.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+
+#   #client_id       = "5cfd5092-8ede-4841-b3fb-56245200ab93"
+#   #client_secret   = "g1F8Q~MaYT8N50lYOJ~LwyzPkGlrX.0y4fgbQa8g"
+#   #tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
+#   #subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
+# }
