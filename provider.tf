407231a0a4cc400827a07436bc951d6c16270284
prodyut <prodyut@kloudgurdian.com>
2026-03-21 16:57:51 +0530

    udpate version

--- provider.tf
+++ provider.tf
@@ -66,10 +66,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 3.0"
-    }
   }
 
   required_version = ">= 1.1.0"
